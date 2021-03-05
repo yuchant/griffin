1b1a30fcdf30059b620a76092d44c2d3978bb0cc
Yuji Tomita <yuji@department.nyc>
2021-03-04 20:56:01 -0800

    save

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 const Page = (props) => {
-  return <div>Griffin</div>;
+  return <div>Griffin: {props.static}</div>;
+};
+
+export const getStaticProps = () => {
+  // <--
+  // this can be async// next doesn't care what you do in here..
+  // the output gets saved as static on build time
+  return {
+    props: {
+      static: Math.random(),
+    },
+  };
 };
 
 export default Page;
